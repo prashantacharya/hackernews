d8837f33bee6f4406377fe07cedc89e926e61a65
Prashant Acharya <prashant.acharya@sagarmatha.edu.np>
2020-08-02 18:33:18 +0545

    Updates

---
+++ precache-manifest.4fde6ee63faaf9376117c93dd2a86ee5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad4ffe8659d24ac5ad30b6427a40b0db",
+    "url": "/hackernews/index.html"
+  },
+  {
+    "revision": "cb7c342dfab6e4101e4a",
+    "url": "/hackernews/static/css/main.78048a7a.chunk.css"
+  },
+  {
+    "revision": "03e6c93b9aa54b7b7c8b",
+    "url": "/hackernews/static/js/2.4eba907e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hackernews/static/js/2.4eba907e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb7c342dfab6e4101e4a",
+    "url": "/hackernews/static/js/main.34fc7073.chunk.js"
+  },
+  {
+    "revision": "c2b6706f847660835a7c",
+    "url": "/hackernews/static/js/runtime-main.0bf9feb2.js"
+  }
+]);
